30fdbc55398e60071edc21e30d58d56bf384725e
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2001-08-01 04:21:51 +0000

    First commit of working code

--- src/FTVectorGlyph.h
+++ src/FTVectorGlyph.h
@@ -1,11 +1,72 @@
 #ifndef		__FTVectorGlyph__
 #define		__FTVectorGlyph__
 
+#include <vector>
+
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 
 #include "FTGlyph.h"
+
+class FTContour
+{
+	public:
+		// methods
+		FTContour();
+		~FTContour();
+	
+		void AddPoint( int x, int y);
+		
+		int size() const { return pointList.size();}
+
+		// attributes
+		
+		typedef pair<int, int> ftPoint;
+		vector< ftPoint> pointList;
+		float ctrlPtArray[4][2];
+	private:
+		// methods
+
+		// attributes
+};
+
+//FIXME get rid of this
+#define MAX_DEG 4
+
+class FTVectoriser
+{
+	public:
+		// methods
+		FTVectoriser( FT_Glyph glyph);
+		virtual ~FTVectoriser();
+		
+		bool Ingest();
+		void Output( float* d);
+		int points();
+		int contours() const { return contourList.size();}
+		int contourSize( int c) const { return contourList[c]->size();}
+		
+		// attributes
+		
+	private:
+		// methods
+		int Conic( int index, int first, int last);
+		int Cubic( int index, int first, int last);
+		void deCasteljau(GLint m, GLfloat t, int n, GLfloat bValues[MAX_DEG][MAX_DEG][2]);
+		void evaluateCurve( int n);
+
+		// attributes
+//		typedef pair<int, int> ftPoint;
+//		vector< FT_Vector> pointList;
+		vector< FTContour*> contourList;
+		float ctrlPtArray[4][2];
+			
+		FTContour* contour;
+
+		FT_Outline ftOutline;
+
+};
 
 
 class	FTVectorGlyph : public FTGlyph
@@ -14,10 +75,20 @@ class	FTVectorGlyph : public FTGlyph
 		// methods
 		FTVectorGlyph( FT_Glyph glyph, int glyphIndex);
 		virtual ~FTVectorGlyph();
-		virtual float Render();
+		virtual float Render( FT_Vector& pen);
 		
 		// attributes
+	
+	private:
+		// methods
 		
+		// attributes
+		FTVectoriser* vectoriser;
+		int numPoints;
+		int numContours;
+		int * contourLength;
+		float* data;
+	
 };
 
 
