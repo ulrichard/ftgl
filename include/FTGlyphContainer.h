69818dda4f45d7694e4f02ef13e65710643a3024
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2001-08-27 03:16:28 +0000

    Started to add comments

--- include/FTGlyphContainer.h
+++ include/FTGlyphContainer.h
@@ -13,15 +13,37 @@ class FTGlyph;
 
 using namespace std;
 
+/**
+ * FTGlyphContainer
+ *
+ * @see FTGlyph
+ */
 class	FTGlyphContainer
 {
 	public:
 		// methods
+
+		/**
+		 * Constructor
+		 */
 		FTGlyphContainer( FTFace* face, int numGlyphs, bool p = false);
+
+		/**
+		 * Destructor
+		 */
 		~FTGlyphContainer();
 
-		bool Add( FTGlyph* tempGlyph);
+		/**
+		 * Adds a glyph to this glyph list.
+		 *
+		 * @param glyph		
+		 * @return			<code>true</code>
+		 */
+		bool Add( FTGlyph* glyph);
 
+		/**
+		 * renders a character
+		 */
 		FT_Vector& render( unsigned int index, unsigned int next, FT_Vector pen);
 		
 		// attributes
@@ -30,19 +52,45 @@ class	FTGlyphContainer
 		// methods
 		
 		// attributes
+
+		/**
+		 *
+		 */
 		FT_Error err;
+
+		/**
+		 *
+		 */
 		bool preCache;
+
+		/**
+		 *
+		 */
 		int numGlyphs;
+
+		/**
+		 *
+		 */
 		FTFace* face;
 		
+
+		/**
+		 *
+		 */
 		FT_Vector kernAdvance;
+
+		/**
+		 *
+		 */
 		float advance;
 
-		
+		/**
+		 *
+		 */
+		vector<FTGlyph*> glyphs;
 //		typedef pair<int, FTGlyph*> CHARREF; // glyphIndex, glyph
 //		vector<CHARREF> glyphs;
 //		map< int, FTGlyph*> CHARREF; // charCode, glyph
-		vector<FTGlyph*> glyphs;
 		
 };
 
