1a6d8d57e11464babb2da25f85044eeb87e9f2d6
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2001-08-28 05:07:52 +0000

    Reversed the test for glyph format == oultline

--- src/FTVectorGlyph.cpp
+++ src/FTVectorGlyph.cpp
@@ -15,50 +15,50 @@ FTVectorGlyph::FTVectorGlyph( FT_Glyph glyph, unsigned int gi)
 	data(0),
 	glList(0)
 {
-	if( glyph->format == ft_glyph_format_outline)
+	if( ft_glyph_format_outline != glyph->format)
+	{ return;}
+
+	vectoriser = new FTVectoriser( glyph);
+	
+	if( vectoriser->Ingest())
 	{
-		vectoriser = new FTVectoriser( glyph);
+		numContours = vectoriser->contours();
 		
-		if( vectoriser->Ingest())
+		contourLength = new int[ numContours];
+		
+		for( int c = 0; c < numContours; ++c)
 		{
-			numContours = vectoriser->contours();
-			
-			contourLength = new int[ numContours];
-			
-			for( int c = 0; c < numContours; ++c)
-			{
-				contourLength[c] = vectoriser->contourSize( c);
-			}
-			
-			numPoints = vectoriser->points();
-			data = new double[ numPoints * 3];
-			vectoriser->Output( data);
-			
-			advance = glyph->advance.x >> 16; // this is 6 in the freetype docs!!!!!!
+			contourLength[c] = vectoriser->contourSize( c);
 		}
-	
-		delete vectoriser;
 		
-	    if ( ( numContours < 1) || ( numPoints < 3)) // check this
-			return;
-			
- 		glList = glGenLists(1);
- 		int d = 0;
- 
- 		glNewList( glList, GL_COMPILE);
- 			for( int c = 0; c < numContours; ++c)
- 			{
- 				glBegin( GL_LINE_LOOP);
- 				for( int p = 0; p < ( contourLength[c]); ++p)
- 				{
- 					glVertex2dv( data + d);
- 					d += 3;
- 				}
- 				glEnd();
- 			}
- 		glEndList();
-	
+		numPoints = vectoriser->points();
+		data = new double[ numPoints * 3];
+		vectoriser->Output( data);
+		
+		advance = glyph->advance.x >> 16; // this is 6 in the freetype docs!!!!!!
 	}
+
+	delete vectoriser;
+	
+	if ( ( numContours < 1) || ( numPoints < 3)) // check this
+		return;
+		
+	glList = glGenLists(1);
+	int d = 0;
+
+	glNewList( glList, GL_COMPILE);
+		for( int c = 0; c < numContours; ++c)
+		{
+			glBegin( GL_LINE_LOOP);
+			for( int p = 0; p < ( contourLength[c]); ++p)
+			{
+				glVertex2dv( data + d);
+				d += 3;
+			}
+			glEnd();
+		}
+	glEndList();
+
 }
 
 
