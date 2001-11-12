d233facc3cbf425a5816100f16e7317f1a788277
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2001-11-12 02:45:11 +0000

    Set the bounding box

--- src/FTPolyGlyph.cpp
+++ src/FTPolyGlyph.cpp
@@ -10,7 +10,9 @@ FTPolyGlyph::FTPolyGlyph( FT_Glyph glyph)
 	glList(0)
 {
 	if( ft_glyph_format_outline != glyph->format)
-	{ return;}
+	{
+		return;
+	}
 
 	vectoriser = new FTVectoriser( glyph);
 	
@@ -26,16 +28,11 @@ FTPolyGlyph::FTPolyGlyph( FT_Glyph glyph)
 	}
 	
 	data = new double[ numPoints * 3];
-
 	vectoriser->GetMesh( data);
-	
-	advance = glyph->advance.x >> 16;
-
 	delete vectoriser;
 	
-	glList = glGenLists(1);
 	int d = 0;
-
+	glList = glGenLists(1);
 	glNewList( glList, GL_COMPILE);
 		int BEPairs = data[0];
 		for( int i = 0; i < BEPairs; ++i)
@@ -56,6 +53,9 @@ FTPolyGlyph::FTPolyGlyph( FT_Glyph glyph)
 
 	delete [] data; // FIXME
 	data = 0;
+
+	bBox = FTBBox( glyph);
+	advance = glyph->advance.x >> 16;
 
 	// discard glyph image (bitmap or not)
 	FT_Done_Glyph( glyph); // Why does this have to be HERE
