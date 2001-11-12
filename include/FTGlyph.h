a97ba0f26fd1927809b803faf5ec94896acbd8d5
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2001-11-12 02:41:29 +0000

    Added FTBbox

--- include/FTGlyph.h
+++ include/FTGlyph.h
@@ -7,7 +7,53 @@
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 
-//#include "FTGL.h"
+
+class FTGL_EXPORT FTBBox
+{
+	public:
+		FTBBox()
+		:	x1(0),
+			y1(0),
+			z1(0),
+			x2(0),
+			y2(0),
+			z2(0)
+		{}
+		
+		FTBBox( FT_Glyph glyph)
+		{
+			FT_BBox bbox;
+			FT_Glyph_Get_CBox( glyph, ft_glyph_bbox_subpixels, &bbox );
+			
+			x1 = bbox.xMin >> 6;
+			y1 = bbox.yMin >> 6;;
+			z1 = 0;
+			x2 = bbox.xMax >> 6;;
+			y2 = bbox.yMax >> 6;;
+			z2 = 0;	
+		}		
+		
+		FTBBox( int a, int b, int c, int d, int e, int f)
+		:	x1(a),
+			y1(b),
+			z1(c),
+			x2(d),
+			y2(e),
+			z2(f)
+		{}
+
+		~FTBBox()
+		{}
+	
+		// Make these ftPoints
+		int x1, y1, z1, x2, y2, z2;
+
+	protected:
+	
+	
+	private:
+};
+
 
 /**
  * FTGlyph is the base class for FTGL glyphs.
@@ -48,6 +94,13 @@ class FTGL_EXPORT FTGlyph
 		float Advance() const { return advance;}
 		
 		/**
+		 * Return the bounding box for this glyph.
+		 *
+		 * @return	bounding box.
+		 */
+		FTBBox BBox() const { return bBox;}
+		
+		/**
 		 * Queries for errors.
 		 *
 		 * @return	The current error code.
@@ -65,6 +118,12 @@ class FTGL_EXPORT FTGlyph
 		 */
 		FT_Vector pos;
 
+		
+		/**
+		 * A freetype bounding box
+		 */
+		FTBBox bBox;
+		
 		/**
 		 * Current error code. Zero means no error.
 		 */
