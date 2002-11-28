c4e1b0fda10e4e3640e8d375e502380de802a4c8
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2002-11-28 09:40:05 +0000

    Got rid of the tabs

--- src/FTGlyphContainer.cpp
+++ src/FTGlyphContainer.cpp
@@ -1,80 +1,80 @@
-#include	"FTGlyphContainer.h"
-#include	"FTGlyph.h"
-#include	"FTFace.h"
+#include    "FTGlyphContainer.h"
+#include    "FTGlyph.h"
+#include    "FTFace.h"
 
 FTGlyphContainer::FTGlyphContainer( FTFace* f, unsigned int g, bool p)
-:	preCache( p),
-	numGlyphs( g),
-	face( f),
-	err( 0)
+:   preCache( p),
+    numGlyphs( g),
+    face( f),
+    err( 0)
 {
-	// Fill the glyphlist with null glyphs
-	glyphs.resize( g, NULL);
+    // Fill the glyphlist with null glyphs
+    glyphs.resize( g, NULL);
 }
 
 
 FTGlyphContainer::~FTGlyphContainer()
 {
-	GlyphVector::iterator iter;
-	for( iter = glyphs.begin(); iter != glyphs.end(); ++iter)
-	{
-		if( *iter)
-		{
-			delete *iter;
-		}
-	}
-	
-	glyphs.clear();
+    GlyphVector::iterator iter;
+    for( iter = glyphs.begin(); iter != glyphs.end(); ++iter)
+    {
+        if( *iter)
+        {
+            delete *iter;
+        }
+    }
+    
+    glyphs.clear();
 }
 
 
 bool FTGlyphContainer::Add( FTGlyph* tempGlyph, unsigned int g)
 {
-	glyphs[g] = tempGlyph;
-	return true;
+    glyphs[g] = tempGlyph;
+    return true;
 }
 
 
 FTGlyph* FTGlyphContainer::Glyph( const unsigned int c) const
 {
-	return glyphs[face->CharIndex( c)];
+    return glyphs[face->CharIndex( c)];
 }
 
 
 FTBBox FTGlyphContainer::BBox( const unsigned int index) const
 {
-	return glyphs[face->CharIndex( index)]->BBox();
+    return glyphs[face->CharIndex( index)]->BBox();
 }
 
 
 float FTGlyphContainer::Advance( unsigned int index, unsigned int next)
 {
-	unsigned int left = face->CharIndex( index);
-	unsigned int right = face->CharIndex( next);
-	
-	float width = face->KernAdvance( left, right).x;
-	width += glyphs[left]->Advance();
-	
-	return width;
+    unsigned int left = face->CharIndex( index);
+    unsigned int right = face->CharIndex( next);
+    
+    float width = face->KernAdvance( left, right).x;
+    width += glyphs[left]->Advance();
+    
+    return width;
 }
 
 
 FTPoint FTGlyphContainer::render( unsigned int index, unsigned int next, FTPoint pen)
 {
-	FTPoint kernAdvance;
+    FTPoint kernAdvance;
     float advance = 0;
-	
-	unsigned int left = face->CharIndex( index);
-	unsigned int right = face->CharIndex( next);
-	
-	kernAdvance = face->KernAdvance( left, right);
-		
-	if( !face->Error())
-	{
-		advance = glyphs[left]->Render( pen);
-	}
-	
-	kernAdvance.x = advance + kernAdvance.x;
-//	kernAdvance.y = advance.y + kernAdvance.y;
-	return kernAdvance;
+    
+    unsigned int left = face->CharIndex( index);
+    unsigned int right = face->CharIndex( next);
+    
+    kernAdvance = face->KernAdvance( left, right);
+        
+    if( !face->Error())
+    {
+        advance = glyphs[left]->Render( pen);
+    }
+    
+    kernAdvance.x = advance + kernAdvance.x;
+//  kernAdvance.y = advance.y + kernAdvance.y;
+    return kernAdvance;
 }
