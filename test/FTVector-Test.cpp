4108dce8cdddb9236106d5f9eda130deb9037b57
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2002-12-20 22:23:29 +0000

    Started writing tests

--- test/FTVector-Test.cpp
+++ test/FTVector-Test.cpp
@@ -10,6 +10,9 @@ class FTVectorTest : public CppUnit::TestCase
 {
         CPPUNIT_TEST_SUITE( FTVectorTest);
             CPPUNIT_TEST( testConstructor);
+            CPPUNIT_TEST( testReserve);
+            CPPUNIT_TEST( testPushBack);
+            CPPUNIT_TEST( testOperatorSquareBrackets);
         CPPUNIT_TEST_SUITE_END();
         
     public:
@@ -20,6 +23,52 @@ class FTVectorTest : public CppUnit::TestCase
 
         void testConstructor()
         {
+            FTVector<float> floatVector;
+
+            CPPUNIT_ASSERT( floatVector.size() == 0);
+            CPPUNIT_ASSERT( floatVector.empty());
+            CPPUNIT_ASSERT( floatVector.capacity() == 0);
+        }
+        
+        
+        void testReserve()
+        {
+            FTVector<float> floatVector;
+
+            floatVector.reserve(128);
+            CPPUNIT_ASSERT( floatVector.capacity() == 256);
+            CPPUNIT_ASSERT( floatVector.empty());
+            CPPUNIT_ASSERT( floatVector.size() == 0);
+        }
+        
+        
+        void testPushBack()
+        {
+            FTVector<int> integerVector;
+
+            CPPUNIT_ASSERT( integerVector.size() == 0);
+            
+            integerVector.push_back(0);
+            integerVector.push_back(1);
+            integerVector.push_back(2);
+            integerVector.push_back(3);
+            
+            CPPUNIT_ASSERT( !integerVector.empty());
+            CPPUNIT_ASSERT( integerVector.size() == 4);
+        }
+        
+        
+        void testOperatorSquareBrackets()
+        {
+            FTVector<int> integerVector;
+
+            integerVector.push_back(1);
+            integerVector.push_back(2);
+            integerVector.push_back(4);
+            integerVector.push_back(8);
+            
+            CPPUNIT_ASSERT( integerVector[0] == 1);
+            CPPUNIT_ASSERT( integerVector[2] == 4);
         }
         
         
