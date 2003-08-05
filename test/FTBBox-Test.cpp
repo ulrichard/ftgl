2105ac7a8d39c27bbe0434c534ea2acf937326ca
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2003-08-05 00:26:54 +0000

    Refactored variable names

--- test/FTBBox-Test.cpp
+++ test/FTBBox-Test.cpp
@@ -29,14 +29,14 @@ class FTBBoxTest : public CppUnit::TestCase
 
         void testDefaultConstructor()
         {
-            FTBBox boundingBox1;
+            FTBBox boundingBox;
 
-            CPPUNIT_ASSERT( boundingBox1.lowerX == 0.0f);
-            CPPUNIT_ASSERT( boundingBox1.lowerY == 0.0f);
-            CPPUNIT_ASSERT( boundingBox1.lowerZ == 0.0f);
-            CPPUNIT_ASSERT( boundingBox1.upperX == 0.0f);
-            CPPUNIT_ASSERT( boundingBox1.upperY == 0.0f);
-            CPPUNIT_ASSERT( boundingBox1.upperZ == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.lowerX == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.lowerY == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.lowerZ == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.upperX == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.upperY == 0.0f);
+            CPPUNIT_ASSERT( boundingBox.upperZ == 0.0f);
         }
         
         
@@ -44,14 +44,14 @@ class FTBBoxTest : public CppUnit::TestCase
         {    
             setUpFreetype();
 
-            FTBBox boundingBox2( glyph);
+            FTBBox boundingBox( glyph);
 
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(   2, boundingBox2.lowerX, 0.01);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL( -15, boundingBox2.lowerY, 0.01);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(   0, boundingBox2.lowerZ, 0.01);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(  35, boundingBox2.upperX, 0.01);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(  38, boundingBox2.upperY, 0.01);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(   0, boundingBox2.upperZ, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(   2, boundingBox.lowerX, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL( -15, boundingBox.lowerY, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(   0, boundingBox.lowerZ, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(  35, boundingBox.upperX, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(  38, boundingBox.upperY, 0.01);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(   0, boundingBox.upperZ, 0.01);
 
             tearDownFreetype();
         }     
@@ -60,7 +60,7 @@ class FTBBoxTest : public CppUnit::TestCase
         {
             FTBBox  boundingBox;
             FTPoint firstMove( 3.5f, 1.0f, -2.5f);
-            FTPoint secondtMove( -3.5f, -1.0f, 2.5f);
+            FTPoint secondMove( -3.5f, -1.0f, 2.5f);
             
             boundingBox.Move( firstMove);
         
@@ -71,7 +71,7 @@ class FTBBoxTest : public CppUnit::TestCase
             CPPUNIT_ASSERT( boundingBox.upperY ==  1.0f);
             CPPUNIT_ASSERT( boundingBox.upperZ == -2.5f);
 
-            boundingBox.Move( secondtMove);
+            boundingBox.Move( secondMove);
         
             CPPUNIT_ASSERT( boundingBox.lowerX == 0.0f);
             CPPUNIT_ASSERT( boundingBox.lowerY == 0.0f);
@@ -127,18 +127,18 @@ class FTBBoxTest : public CppUnit::TestCase
         void setUpFreetype()
         {
             FT_Error error = FT_Init_FreeType( &library);
-            assert(!error);
+            CPPUNIT_ASSERT(!error);
             error = FT_New_Face( library, GOOD_FONT_FILE, 0, &face);
-            assert(!error);
+            CPPUNIT_ASSERT(!error);
             
             long glyphIndex = FT_Get_Char_Index( face, CHARACTER_CODE_G);
             
             FT_Set_Char_Size( face, 0L, FONT_POINT_SIZE * 64, RESOLUTION, RESOLUTION);
             
             error = FT_Load_Glyph( face, glyphIndex, FT_LOAD_DEFAULT);
-            assert(!error);
+            CPPUNIT_ASSERT(!error);
             error = FT_Get_Glyph( face->glyph, &glyph);
-            assert(!error);
+            CPPUNIT_ASSERT(!error);
         }
         
         void tearDownFreetype()
