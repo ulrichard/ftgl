9bfb9a5e3bf455cee3c31af371cc441299510f79
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2003-09-24 22:13:16 +0000

    Initial Add

---
+++ test/FTBitmapGlyph-Test.cpp
@@ -0,0 +1,75 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <assert.h>
+
+#include "Fontdefs.h"
+#include "FTBitmapGlyph.h"
+
+class FTBitmapGlyphTest : public CppUnit::TestCase
+{
+    CPPUNIT_TEST_SUITE( FTBitmapGlyphTest);
+        CPPUNIT_TEST( testConstructor);
+    CPPUNIT_TEST_SUITE_END();
+        
+    public:
+        FTBitmapGlyphTest() : CppUnit::TestCase( "FTBitmapGlyph Test")
+        {
+        }
+        
+        FTBitmapGlyphTest( const std::string& name) : CppUnit::TestCase(name) {}
+        
+        ~FTBitmapGlyphTest()
+        {
+        }
+        
+        void testConstructor()
+        {
+            FT_Library   library;
+            FT_Face      face;
+            FT_Glyph     glyph;
+            
+            FT_Error error = FT_Init_FreeType( &library);
+            assert(!error);
+            error = FT_New_Face( library, FONT_FILE, 0, &face);
+            assert(!error);
+            
+            FT_Set_Char_Size( face, 0L, FONT_POINT_SIZE * 64, RESOLUTION, RESOLUTION);
+            
+            error = FT_Load_Char( face, CHARACTER_CODE_A, FT_LOAD_DEFAULT);
+            assert( !error);
+            error = FT_Get_Glyph( face->glyph, &glyph);
+            assert( !error);
+            assert( !error);
+        
+            makeBitMap( glyph);
+            
+//            FTBitmapGlyph* bitmapGlyph = new FTBitmapGlyph( &glyph);            
+//            CPPUNIT_ASSERT( bitmapGlyph->Error() == 0);
+        
+            FT_Done_Glyph( glyph);
+            FT_Done_Face( face);
+            FT_Done_FreeType( library);
+            
+        }
+
+            
+        void makeBitMap( FT_Glyph glyph)
+        {
+            FT_Error error = FT_Glyph_To_Bitmap( &glyph, FT_RENDER_MODE_MONO, 0, 0);
+            assert( !error);
+//            FT_Done_Glyph( glyph);
+        }
+        
+        void setUp() 
+        {}
+        
+        void tearDown() 
+        {}
+                    
+    private:
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( FTBitmapGlyphTest);
+
