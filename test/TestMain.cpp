bf7497fb6cd96c6b9707783caaf3e1c91618122e
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2004-09-28 02:39:58 +0000

    Added a function to construct a gl context. Required for some tests

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -1,6 +1,12 @@
 #include <cppunit/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 
+#ifdef __APPLE_CC__
+	#include <GLUT/glut.h>
+#else
+	#include <GL/glut.h>
+#endif
+
 int main(int argc, const char* argv[])
 {
     CppUnit::TextTestRunner runner;
@@ -9,4 +15,23 @@ int main(int argc, const char* argv[])
     runner.run();
 
     return 0;
+}
+
+
+void buildGLContext()
+{
+	static bool glutInitialised = false;
+    char* pointer;
+    int number;
+    
+    if( !glutInitialised)
+    {
+        glutInit( &number, &pointer);
+        glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+        glutInitWindowPosition(0, 0);
+        glutInitWindowSize( 0, 0);
+        glutCreateWindow("FTGL TEST");
+        
+        glutInitialised = true;
+    }
 }
