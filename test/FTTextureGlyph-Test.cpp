9ec7742426718704bdca363dbababa04ef9d42a0
henry <henry@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2004-09-29 04:13:21 +0000

    Constructed texture to stop glError in test.

--- test/FTTextureGlyph-Test.cpp
+++ test/FTTextureGlyph-Test.cpp
@@ -42,8 +42,21 @@ class FTTextureGlyphTest : public CppUnit::TestCase
             assert( !error);
             
             buildGLContext();
+            
+            GLuint textureID;
+            glGenTextures(1, &textureID);
+            
+            char* texture[64*64];
         
-            FTTextureGlyph* textureGlyph = new FTTextureGlyph( face->glyph, 0, 0, 0, 0, 0);
+            glBindTexture( GL_TEXTURE_2D, textureID);
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+            glTexImage2D( GL_TEXTURE_2D, 0, GL_ALPHA, 64, 64, 0, GL_ALPHA, GL_UNSIGNED_BYTE, texture);
+    
+            FTTextureGlyph* textureGlyph = new FTTextureGlyph( face->glyph, textureID, 0, 0, 64, 64);
          
             CPPUNIT_ASSERT( textureGlyph->Error() == 0);
         
