d9cdef45ab204c76223eac4003a239d5c4be54f3
sammy <sammy@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2008-05-02 09:13:14 +0000

    * Add RENDER_ALL to the enum used in Font::Render() so that client     applications need not worry about future extensions.

--- src/FTGL/ftgl.h
+++ src/FTGL/ftgl.h
@@ -64,6 +64,7 @@ namespace FTGL
         RENDER_FRONT = 0x01,
         RENDER_BACK = 0x02,
         RENDER_SIDE = 0x04,
+        RENDER_ALL = 0xffff,
     } RenderMode;
 
     typedef enum
@@ -75,9 +76,11 @@ namespace FTGL
     } TextAlignment;
 }
 #else
-#   define FTGL_RENDER_FRONT  0x01
-#   define FTGL_RENDER_BACK   0x02
-#   define FTGL_RENDER_SIDE   0x04
+#   define FTGL_RENDER_FRONT  0x0001
+#   define FTGL_RENDER_BACK   0x0002
+#   define FTGL_RENDER_SIDE   0x0004
+#   define FTGL_RENDER_ALL    0xffff
+
 #   define FTGL_ALIGN_LEFT    0
 #   define FTGL_ALIGN_CENTER  1
 #   define FTGL_ALIGN_RIGHT   2
