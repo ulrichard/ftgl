6ba54b3d267568584912da56c631bbe39736ea3a
dtremenak <dtremenak@74be908f-6f43-0410-a0eb-e6a2fe4e4ee7>
2008-05-08 23:31:55 +0000

    disable "'this': used in base member initializer list" warning.  although it is dangerous practice, it's valid and ftgl does not use the passed pointer until well after the object is guaranteed to be fully constructed.

--- msvc/config.h
+++ msvc/config.h
@@ -3,3 +3,8 @@
 
 // M_PI and friends on VC
 #define _USE_MATH_DEFINES
+
+// quell spurious "'this': used in base member initializer list" warnings
+#ifdef _MSC_VER
+#pragma warning(disable: 4355)
+#endif
