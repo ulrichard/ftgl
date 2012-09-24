9289260df9ba0d2fa2494ed9c94a3a627beee955
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2012-09-24 21:10:16 +0200

    another try

--- ppa_upload.sh
+++ ppa_upload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # build a debian sourcepackage and upload it to the launchpad ppa
-for DISTRIBUTION in precise # oneiric natty maverick 
+for DISTRIBUTION in precise quantal # oneiric natty maverick 
 do
-	FTGLVERSIONSTR=2.1.3~rc5-8polyextr~${DISTRIBUTION}
+	FTGLVERSIONSTR=2.1.3~rc5-9polyextr~${DISTRIBUTION}
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
 #	rm -rf debian/source
 	dpkg-buildpackage -rfakeroot -k${GPGKEY} -S
